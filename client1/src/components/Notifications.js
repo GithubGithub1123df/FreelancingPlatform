5aa809290b6882dffa2cda2410dcffd11301e9df
GithubGithub1123df <bini6126@gmail.com>
2025-06-03 11:17:42 +0300

    updating pages

--- client1/src/components/Notifications.js
+++ client1/src/components/Notifications.js
@@ -6,12 +6,78 @@ const apiUrl = process.env.REACT_APP_API_URL;
 function Notifications() {
   const [Announcements, setAnnouncements] = useState([]);
   const [activeAnnouncement, setActiveAnnouncement] = useState(null);
+  const deleteAnnouncement = async (id) => {
+    try {
+      const res = await fetch(`${apiUrl}/AdminAnnouncement/${id}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        alert("Deleted successfully");
+
+        setAnnouncements((prev) => prev.filter((item) => item._id !== id));
+      } else {
+        alert(data.message || "Failed to delete");
+      }
+    } catch (err) {
+      console.error(err);
+      alert("Error deleting announcement");
+    }
+  };
+
   useEffect(() => {
     axios
-      .get(`${apiUrl}/notifications`)
+      .get(`${apiUrl}/AdminAnnouncement`)
       .then((res) => setAnnouncements(res.data))
       .catch((err) => console.error("Failed to fetch users:", err));
   }, []);
+
+  const [form, setForm] = useState({
+    title: "",
+    announce: "",
+  });
+  const [msg, setMsg] = useState("");
+  const nav = useNavigate();
+
+  const onChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${apiUrl}/AdminAnnouncement`, form, {
+        withCredentials: true,
+      });
+      setMsg("Feedback Received");
+      setTimeout(() => nav("/"), 1500);
+    } catch (err) {
+      setMsg(err.response?.data?.message || "Error");
+    }
+  };
+
+  // this is for the search part
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/AdminAnnouncement`)
+      .then((res) => res.json())
+      .then((Announcements) => {
+        setData(Announcements);
+        setResults(Announcements);
+      });
+  }, []);
+
+  useEffect(() => {
+    const filtered = data.filter((announcement) =>
+      [announcement.title, announcement.announce].some((field) =>
+        field?.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+    setResults(filtered);
+  }, [searchTerm, data]);
   return (
     <>
       <div className="d-flex align-items-center justify-content-spaceBetween bg-white flex-column vh-100">
